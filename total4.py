273d20db869251ea6aead87370590d07f33e0311
SujivaKauZhenYip <sujivaalt1002@gmail.com>
2026-01-30 14:45:00 +0800

    Added the main calculator files

---
+++ total4.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+E_RATE = 1.00  
+W_RATE = 2.00  
+
+@app.route('/', methods=['GET', 'POST'])
+def calculate_all():
+    e_usage = 0.0
+    w_usage = 0.0
+    total_price = 0.0
+    
+    if request.method == 'POST':
+        try:
+            e_usage = float(request.form.get('e_usage', 0) or 0)
+            w_usage = float(request.form.get('w_usage', 0) or 0)
+
+            total_price = (e_usage * E_RATE) + (w_usage * W_RATE)
+        except ValueError:
+            pass
+
+    return render_template('Total4.html', 
+                           e_usage=e_usage, 
+                           w_usage=w_usage, 
+                           total_price=total_price)
+
+if __name__ == '__main__':
+    app.run(port=5004, debug=True)
